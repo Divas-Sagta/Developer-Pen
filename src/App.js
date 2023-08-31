f513923154188832d8fed60004e6cc53aa7fb6a2
Divas-Sagta <divassagta19@gmail.com>
2023-09-01 01:33:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './components/Home'
+import DataProvider from './context/DataProvider';
+const App = () => {
+  return (
+    <DataProvider>    
+      <Home/>
+    </DataProvider>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Code from './Code';
+import Result from './Result';
+import Header from './Header';
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Code/>
+    <Result/>
+    </>
+  )
+}
+
+export default Home
